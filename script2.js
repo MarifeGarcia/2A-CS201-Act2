f54cfcf724bfaf3b008aea1ee7d2b28a917c34f0
MarifeGarcia <147678876+MarifeGarcia@users.noreply.github.com>
2023-10-18 15:17:05 +0800

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,39 @@
+//Activity 2
+let firstFavNum = 4, secondFavNum = 20;
+
+firstFavNum > secondFavNum
+
+  console.log("firstFavNum is greater than secondFavnum");
+  
+ console.log("firstFavNum is less than secondFavnum");
+
+  console.log("firstFavNum is greater than equal to secondFavnum");
+
+
+  console.log("firstFavNum is less than equal to secondFavnum");
+  
+   firstFavNum = secondFavNum
+
+  console.log("firstFavNum is less than equal to secondFavnum");
+
+  console.log("firstFavNum is less than equal to secondFavnum");
+
+
+  firstFavNum == secondFavNum
+
+  console.log("firstFavNum is less than equal to secondFavnum");
+
+
+   firstFavNum !== secondFavNum
+
+
+
+  console.log("firstFavNum is less than equal to secondFavnum");
+
+
+
+  console.log("firstFavNum is less than equal to secondFavnum");
+  
+  
+  
+  console.log("firstFavNum is equal to secondFavnum");
